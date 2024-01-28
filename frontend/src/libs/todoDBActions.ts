e7cf13973bf946df2f1e615c7da27b7ff76e07b6
Chen <chenzg12345@gmail.com>
2024-01-28 19:19:09 +0200

    - made allow origin specific to app - fixed deleteTodo path in backend - implemented reload on delete to do - implemented all backend actions in server mode i.e "use server"

--- frontend/src/libs/todoDBActions.ts
+++ frontend/src/libs/todoDBActions.ts
@@ -6,14 +6,48 @@ import CDate from "./CDate";
 export async function addTodo(formValues: Inputs) {
     "use server";
 
-    fetch("http://server:8080/addTodo",
-    {
-        method: "POST",
-        body: JSON.stringify({
-            todo: formValues.todo,
-            urgency: Number(formValues.urgency),
-            date: new CDate(formValues.date).toDateTime(),
-        }),
-        cache: "no-store",
-    });
+    try {
+        fetch("http://server:8080/addTodo",
+        {
+            method: "POST",
+            body: JSON.stringify({
+                todo: formValues.todo,
+                urgency: Number(formValues.urgency),
+                date: new CDate(formValues.date).toDateTime(),
+            }),
+            cache: "no-store",
+        });
+    } catch(e: any) {
+        throw new Error(e)
+    }
+}
+
+export async function finishTodos(ids: number[]) {
+    "use server";
+
+    try {
+        fetch("http://server:8080/finishTodos",
+        {
+            method: "POST",
+            body: JSON.stringify({ ids }),
+            cache: "no-store",
+        });
+    } catch(e: any) {
+        throw new Error(e)
+    }
+}
+
+export async function deleteTodo(id: string) {
+    "use server";
+
+    try {
+        fetch("http://server:8080/deleteTodo",
+        {
+            method: "POST",
+            body: JSON.stringify({ id }),
+            cache: "no-store",
+        });
+    } catch(e: any) {
+        throw new Error(e)
+    }
 }

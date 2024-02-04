245c580d33707603381a86576a7699953e432a4f
Chen <chenzg12345@gmail.com>
2024-02-04 17:32:53 +0200

    - added docker secrets for security - implemented input validation code

--- backend/main.go
+++ backend/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"project/internal/database"
 	"project/internal/todoEndpoint"
 	"project/internal/usersEndpoint"
@@ -13,11 +15,47 @@ import (
 // Port we listen on.
 const portNum string = ":8080"
 
+func loadEnv() (string, string, string) {
+	var db_user_file = os.Getenv("DB_USER_FILE")
+	var db_database_file = os.Getenv("DB_DATABASE_FILE")
+	var db_password_file = os.Getenv("DB_PASSWORD_FILE")
+
+	var db_database []byte
+	var db_password []byte
+
+	db_user, err := os.ReadFile(db_user_file)
+
+	if err != nil {
+		log.Panic("Error reading db user file")
+	}
+
+	db_database, err = os.ReadFile(db_database_file)
+
+	if err != nil {
+		log.Panic("Error reading db database file")
+	}
+
+	db_password, err = os.ReadFile(db_password_file)
+
+	if err != nil {
+		log.Panic("Error reading db password file")
+	}
+
+	return string(db_user), string(db_password), string(db_database)
+}
+
+func generateConnString() string {
+	user, password, database := loadEnv()
+	return fmt.Sprintf("%s:%s@tcp(mysql:3306)/%s", user, password, database)
+}
+
 func main() {
 	var dbErr error
 
+	connString := generateConnString()
+
 	// init database
-	database.DBcon, dbErr = sql.Open("mysql", "root:password@tcp(mysql:3306)/todo")
+	database.DBcon, dbErr = sql.Open("mysql", connString)
 	if dbErr != nil {
 		log.Panic(dbErr)
 	}

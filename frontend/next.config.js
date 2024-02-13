16546fc26b3cd130af336315e0f664f750aa97b5
Chen <chenzg12345@gmail.com>
2024-02-13 14:48:15 +0200

    - updated site headers per mozilla's observatory suggestions for improved xss prevention and overall security

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,45 @@
+const cspHeader = `
+    default-src 'self';
+    script-src 'self';
+    style-src 'self';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    block-all-mixed-content;
+    upgrade-insecure-requests;
+`
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+const nextConfig = {
+    async headers() {
+        return [
+            {
+                source: '/(.*)',
+                headers: [
+                    {
+                        key: 'Content-Security-Policy',
+                        value: cspHeader.replace(/\n/g, ''),
+                    },
+                    {
+                        key: "Strict-Transport-Security",
+                        value: "max-age=63072000; includeSubDomains; preload",
+                    },
+                    {
+                        key: "X-Frame-Options",
+                        value: "DENY"
+                    },
+                    {
+                        key: "X-Content-Type-Options",
+                        value: "nosniff"
+                    },
+                ],
+            },
+        ]
+    },
+}
 
 module.exports = nextConfig

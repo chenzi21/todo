8da7fc668a60ec08d800da9eb2a9b03587175c58
chenzi21 <88926016+chenzi21@users.noreply.github.com>
2024-02-26 11:46:25 +0200

    Jenkins (#5)
    * testing jenkins
    
    * changed pem file location

--- frontend/src/app/addToDo/page.tsx
+++ frontend/src/app/addToDo/page.tsx
@@ -2,7 +2,6 @@ import { Suspense } from "react";
 import ToDoFrom from "../../components/addTodo/ToDoForm";
 
 export default function Page() {
-    console.log("test");
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <Suspense fallback={<h2>Loading...</h2>}>

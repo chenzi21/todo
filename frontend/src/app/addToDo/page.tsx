fa16b6005a7ac2f010d948ea3e1ecf584731333b
chenzi21 <88926016+chenzi21@users.noreply.github.com>
2024-02-26 07:14:05 +0200

    testing jenkins (#4)

--- frontend/src/app/addToDo/page.tsx
+++ frontend/src/app/addToDo/page.tsx
@@ -2,11 +2,13 @@ import { Suspense } from "react";
 import ToDoFrom from "../../components/addTodo/ToDoForm";
 
 export default function Page() {
-	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<Suspense fallback={<h2>Loading...</h2>}>
-				<ToDoFrom />
-			</Suspense>
-		</main>
-	);
+    console.log("test");
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <Suspense fallback={<h2>Loading...</h2>}>
+                <ToDoFrom />
+            </Suspense>
+        </main>
+    );
 }
+

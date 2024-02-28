62c18645d82bee6c6bfe02ff28ad74e7f21d577d
Chen <chenzg12345@gmail.com>
2024-02-28 11:16:58 +0200

    debugging logs

--- frontend/src/middleware.tsx
+++ frontend/src/middleware.tsx
@@ -2,7 +2,10 @@ import { NextRequest, NextResponse } from "next/server";
 import { getIsAuthenticated } from "./libs/getIsAuthenticated";
 
 export default async function middleware(request: NextRequest) {
+    console.log("in middleware");
+
     if (request.nextUrl.pathname.startsWith("/_next")) {
+        console.log("here", request.nextUrl);
         return NextResponse.next();
     }
 
@@ -13,6 +16,8 @@ export default async function middleware(request: NextRequest) {
         !request.url.includes("/login") &&
         !request.url.includes("/signup")
     ) {
+        console.log("not signed in", request.url);
+        console.log("is authenticated: ", isAuthenticated);
         return NextResponse.redirect(new URL("/login", request.url));
     }
 

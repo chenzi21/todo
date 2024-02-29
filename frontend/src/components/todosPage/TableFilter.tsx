b65f7b934dad7ae910e4b6d8d3bece447cf15dc1
Chen <chenzg12345@gmail.com>
2024-02-29 12:30:42 +0200

    made filters a bit more responsive for mobile users

--- frontend/src/components/todosPage/TableFilter.tsx
+++ frontend/src/components/todosPage/TableFilter.tsx
@@ -32,27 +32,34 @@ export default function TableFilter({ data, header }: Props) {
             <DropdownMenuTrigger asChild>
                 <FilterIcon className="cursor-pointer" size={ICON_SIZE} />
             </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
+            <DropdownMenuContent className="max-w-[50vw]" align="end">
                 <DropdownMenuLabel>Filter</DropdownMenuLabel>
                 {[
                     ...new Set(
                         data.map((todo) =>
-                            header.column.id in todo
-                                ? todo[
-                                      header.column.id as keyof typeof todo
-                                  ].toString()
-                                : null
+                            header.column.id in todo ? (
+                                <DropdownMenuItem
+                                    key={`${todo.id}_${header.id}`}
+                                    onClick={() =>
+                                        header.column.setFilterValue(
+                                            todo[
+                                                header.column
+                                                    .id as keyof typeof todo
+                                            ].toString()
+                                        )
+                                    }
+                                >
+                                    <p className="whitespace-nowrap overflow-hidden text-ellipsis">
+                                        {todo[
+                                            header.column
+                                                .id as keyof typeof todo
+                                        ].toString()}
+                                    </p>
+                                </DropdownMenuItem>
+                            ) : null
                         )
                     ),
-                ]
-                    .filter((val) => !!val)
-                    .map((val) => (
-                        <DropdownMenuItem
-                            onClick={() => header.column.setFilterValue(val)}
-                        >
-                            {val}
-                        </DropdownMenuItem>
-                    ))}
+                ].filter((val) => !!val)}
             </DropdownMenuContent>
         </DropdownMenu>
     );
